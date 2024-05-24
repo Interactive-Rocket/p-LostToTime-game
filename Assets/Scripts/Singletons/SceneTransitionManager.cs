32bc61724fff99f1bb0a56afb7b2b54f63cd7466
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-05-24 10:02:51 +0200

    Implemented Fading In and Out, changes in Architecture

--- Assets/Scripts/Singletons/SceneTransitionManager.cs
+++ Assets/Scripts/Singletons/SceneTransitionManager.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 public class SceneTransitionManager : MonoBehaviour
 {
     public static SceneTransitionManager Instance { get; set; }
+    [Header("The scene after the current one")]
+    [Tooltip("String name of the scene to load.")]
+    [SerializeField] private string m_sceneToLoad;
+    [Header("Transition animations")]
 
     [Tooltip("The StateMachine of the FadeIn and Out animations")]
     [SerializeField] private Animator m_transitionsAnimator;
@@ -12,6 +16,8 @@ public class SceneTransitionManager : MonoBehaviour
     [SerializeField] private AnimationClip m_fadeIn;
     [Tooltip("The Fade Out animation")]
     [SerializeField] private AnimationClip m_fadOut;
+
+    public float CurrentAnimatorStateTime => m_transitionsAnimator ? m_transitionsAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime : 0f;
 
     void Awake()
     {
@@ -23,10 +29,16 @@ public class SceneTransitionManager : MonoBehaviour
         {
             Instance = this;
         }
+        CheckForSceneManager();
         //Makes sure the fade-in is the first state once the scene starts
         FadeIn();
     }
 
+    private void CheckForSceneManager()
+    {
+        if (SceneManagerSingleton.Instance != null) return;
+        Debug.LogError("The SceneTransitionManager needs a SceneManager to work, add it to the scene");
+    }
     public void FadeIn()
     {
         if (m_transitionsAnimator) m_transitionsAnimator.Play(m_fadeIn.name, 0, 0.0f);
@@ -35,10 +47,11 @@ public class SceneTransitionManager : MonoBehaviour
     public void FadeOut()
     {
         if (m_transitionsAnimator) m_transitionsAnimator.Play(m_fadOut.name, 0, 0.0f);
+        //float t = m_transitionsAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime;
     }
 
-    private void Update() 
+    public void NextScene()
     {
-        //if(m_transitionsAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.name == m_fadOut.name) Debug.Log(m_transitionsAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);    
+        if (SceneManagerSingleton.Instance != null) SceneManagerSingleton.Instance.LoadScene(m_sceneToLoad);
     }
 }
